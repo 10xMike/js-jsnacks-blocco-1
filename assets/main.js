99e5c2dc05cbd0acebc9eb5739fe9190e5a2c7c8
10xMike <michelegec@gmail.com>
2021-04-27 11:54:33 +0200

    Numeri

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,32 @@
-/* var number1 = Number(prompt('inserisci numero'));
-var number2 = Number(prompt('inserisci un numero'));
 
-if (number1 === number2) {
-    console.log('Same number try again');
-} else if (number1 > number2) {
-    console.log(number1);
-} else {
-    console.log(number2);
-} */
 
-// Esercizio 2
+
 // L’utente inserisce due parole in successione, con due prompt.
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 
-var word_1 = prompt('Inserisci una parola')
-var word_2 = prompt('Inserisci una seconda parola')
+/* var word_1 = prompt('Inserisci una parola');
+var word_2 = prompt('Inserisci una seconda parola');
 
 if (word_1.length < word_2.length) {
-    console.log(word_1);
-} else (word_2.length < word_1.length); {
-    console.log(word_2);
-}
+    console.log(word_1, word_2);
+} else if (word_2.length < word_1.length); {
+    console.log(word_2, word_1);
+} else (word_1.length === word_2.length) {
+    console.log(word_1, word_2);
+}  */
 
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
 
+var numero1 = Number(prompt('Inserisci un numero'))
+var numero2 = Number(prompt('Inserisci un numero'))
+var numero3 = Number(prompt('Inserisci un numero'))
+var numero4 = Number(prompt('Inserisci un numero'))
+var numero5 = Number(prompt('Inserisci un numero'))
+var numero6 = Number(prompt('Inserisci un numero'))
+var numero7 = Number(prompt('Inserisci un numero'))
+var numero8 = Number(prompt('Inserisci un numero'))
+var numero9 = Number(prompt('Inserisci un numero'))
+var numero10 = Number(prompt('Inserisci un numero')
 
-
-
-
-
+document.getElementById('somma').innerHTML = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10
