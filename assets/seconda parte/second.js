bd9b8502d0e26ccfe3e1c6659dc75997bad87007
10xMike <michelegec@gmail.com>
2021-05-10 12:15:09 +0200

    Esercizio Triangolo

--- assets/seconda parte/second.js
+++ assets/seconda parte/second.js
@@ -8,3 +8,16 @@ var Palla = {
 // 1b. Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla
 
 Palla.peso =  parseInt(prompt('Modifica il peso della palla'));
+
+// Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza
+// Calcolare perimetro e area
+
+var Triangolo = {
+    base: 20,
+    altezza: 30,
+}
+
+var perimetro = Triangolo.base + Triangolo.altezza + Triangolo.altezza;
+console.log(perimetro);
+var area = ((Triangolo.base * Triangolo.altezza)/2);
+console.log(area);
